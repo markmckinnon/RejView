79aa29a222e92b089917399494d91bb7c2525c1b
Willi Ballenthin <william.ballenthin@mandiant.com>
2013-08-18 21:00:36 -0400

    add HexViewTest

--- src/HexViewTest.java
+++ src/HexViewTest.java
@@ -1,14 +1,11 @@
-import com.williballenthin.RejistryView.RejView;
-import com.williballenthin.rejistry.RegistryHiveFile;
-
+import com.williballenthin.HexView.HexView;
 import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
+import java.nio.ByteBuffer;
 
-public class RejViewTest {
+public class HexViewTest {
     public static void main(String[] args) {
         final String hivePath = args[0];
-        final File hiveFile = new File(hivePath);
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 try {
@@ -27,14 +24,23 @@ public class RejViewTest {
                 JFrame frame = new JFrame("RejViewTest");
                 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-                try {
-                    JComponent pane = new RejView(new RegistryHiveFile(hiveFile));
-                    frame.setContentPane(pane);
-                    frame.pack();
-                    frame.setVisible(true);
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+                ByteBuffer buf = ByteBuffer.allocate(256 * 2);
+                buf.position(0x0);
+                for (int i = 0; i < 255; i++) {
+                    System.out.println("" + i);
+                    buf.put((byte)i);
                 }
+                for (int i = 0; i < 255; i++) {
+                    System.out.println("" + i);
+                    buf.put((byte)i);
+                }
+
+                JComponent pane = new HexView(buf);
+
+                frame.setContentPane(pane);
+                frame.pack();
+                frame.setVisible(true);
             }
         });
     }
