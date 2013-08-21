9903a6caaa5c93691db6bc63bf1c1bc8f25d4755
Willi Ballenthin <william.ballenthin@mandiant.com>
2013-08-20 23:31:07 -0400

    add constructor for ByteBuffers

--- src/com/williballenthin/RejistryView/RejView.java
+++ src/com/williballenthin/RejistryView/RejView.java
@@ -1,9 +1,11 @@
 package com.williballenthin.RejistryView;
 
 import com.williballenthin.rejistry.RegistryHive;
+import com.williballenthin.rejistry.RegistryHiveBuffer;
 
 import javax.swing.*;
 import java.awt.*;
+import java.nio.ByteBuffer;
 
 public class RejView extends JPanel implements RejTreeNodeSelectionListener {
 
@@ -14,10 +16,28 @@ public class RejView extends JPanel implements RejTreeNodeSelectionListener {
     public RejView(RegistryHive hive) {
         super(new BorderLayout());
         this._hive = hive;
-        this._tree_view = new RejTreeView(this._hive);
 
+        // have to do these cause they're final
+        this._tree_view = new RejTreeView(this._hive);
         this._splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
                 this._tree_view, new JPanel());
+
+        this.setupUI();
+    }
+
+    public RejView(ByteBuffer buf) {
+        super(new BorderLayout());
+        this._hive = new RegistryHiveBuffer(buf);
+
+        // have to do these cause they're final
+        this._tree_view = new RejTreeView(this._hive);
+        this._splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                this._tree_view, new JPanel());
+
+        this.setupUI();
+    }
+
+    private void setupUI() {
         this._splitPane.setResizeWeight(0);
         this._splitPane.setOneTouchExpandable(true);
         this._splitPane.setContinuousLayout(true);
